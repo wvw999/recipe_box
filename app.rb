f8204e8e3894f114c10c83cdd935623ccb3490ff
Rita Bennett-Chew and Will Swanson <rita@>
2018-07-19 11:32:49 -0700

    Fix ingredient listing with DRYer ActiveRecord code

--- app.rb
+++ app.rb
@@ -17,24 +17,27 @@ end
 post('/recipe') do
   recipe_title = params["title"]
   @recipe = Recipe.create({:title => recipe_title})
-  @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
+  # want the recipe.ingredients to be passed into erb
+  @recipe_ingredients = @recipe.ingredients
+  # @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
   erb(:edit)
 end
 
 get('/recipes/:id/display') do
   @recipe = Recipe.find(params[:id].to_i)
-  @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
+  # @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
+  @recipe_ingredients = @recipe.ingredients
   erb(:recipe)
 end
 
 patch('/recipes/:id/ingredient') do
   @recipe = Recipe.find(params[:id].to_i)
-
-
   new_ingredient = @recipe.ingredients.create({:ingredient_name => params["new_ingredient"]})
   @amount = Amount.all.last
   @amount.update(amount: params["amount"])
-  @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
+  # @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
+  @recipe_ingredients = @recipe.ingredients
+
   # new_ingredient.amounts.create({:amount => params["amount"], :recipe_id => @recipe.id})
   erb(:edit)
 end
@@ -42,17 +45,20 @@ end
 patch('/recipes/:id/instructions') do
   @recipe = Recipe.find(params[:id])
   @recipe.update({:instructions => params["new_instructions"]})
-  @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
+  # @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
+  @recipe_ingredients = @recipe.ingredients
   erb(:edit)
 end
 
 get('/recipes/:id/edit') do
   @recipe = Recipe.find(params[:id])
-  @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
+  # @ingredients_amounts = @recipe.recipe_with_amount(@recipe.id)
+  @recipe_ingredients = @recipe.ingredients
   erb(:edit)
 end
 
 #
+
 # post('/recipes/:id/remove') do
 #
 # end
